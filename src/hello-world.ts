eaea7dff370ada4f4b7440f4fa6822e74a56b55e
Yev Barkalov <yevbar1729@gmail.com>
2018-12-13 20:24:45 -0800

    Change hello-world.ts

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,9 +1,13 @@
 import * as sourcegraph from 'sourcegraph'
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
+    console.log("We have now activated");
     ctx.subscriptions.add(
         sourcegraph.languages.registerHoverProvider(['*'], {
-            provideHover: () => {console.log("Hello world, this should work!");return { contents: { value: 'Hello world from WIP: My first extension! 🎉🎉🎉' } }}
+            provideHover: () => {
+                console.log("Hello from the hover function");
+                return { contents: { value: "Hello world from my first extension!"}}
+            }
         })
     )
 }
